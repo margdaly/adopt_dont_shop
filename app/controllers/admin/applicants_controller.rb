1664333cd5307edb3610aa6d8146ff28b7951f80
Reilly Miller <reillyjmiller31@gmail.com>
2023-04-04 13:39:45 -0600

    create method in model, update in controller

--- app/controllers/admin/applicants_controller.rb
+++ app/controllers/admin/applicants_controller.rb
@@ -8,13 +8,27 @@ class Admin::ApplicantsController < ApplicationController
     # # pet = Pet.find(params[:id])
   end
 
-  def update
-    @applicant = Applicant.find(params[:id])
-    @pet = Pet.find(params[:pet_id])
-    if params[:commit] == "Approve #{@pet.name} for Adoption"
-      @pet.update({status: "Approved"})
-      @pet.save
-    end
-    redirect_to "/admin/applicants/#{@applicant.id}"
-  end
+  # def update
+  #   @applicant = Applicant.find(params[:id])
+  #   @pet = Pet.find(params[:pet_id])
+  #   if params[:commit] == "Approve #{@pet.name} for Adoption"
+  #     @pet.update({status: "Approved"})
+  #     @pet.save
+  #   end
+  #   redirect_to "/admin/applicants/#{@applicant.id}"
+  # end
+
+  # def update
+  #   @pet_applicant = PetApplicant.find(params[:id])
+  #   @pet = Pet.find(params[:pet_id])
+
+  #   if params[:commit] == "Approve #{@pet.name} for Adoption"
+  #     @pet.update({status: "Approved"})
+  #     @pet.save
+  #   elsif params[:commit] == "Reject #{@pet.name} for Adoption"
+  #     @pet.update({status: "Rejected"})
+  #     @pet.save
+  #   end
+  #   redirect_to "/admin/applicants/#{@applicant.id}"
+  # end
 end
--- app/controllers/pet_applicants_controller.rb
+++ app/controllers/pet_applicants_controller.rb
@@ -1,11 +1,19 @@
 class PetApplicantsController < ApplicationController
   def create
     pet_applicant = PetApplicant.create(pet_applicant_params)
+    applicant = pet_applicant.applicant
     redirect_to "/applicants/#{pet_applicant.applicant_id}"
+  end
+
+  def update
+    pet_applicant = PetApplicant.find(params[:id])
+    pet_applicant.update(pet_applicant_params)
+    applicant = pet_applicant.applicant
+    redirect_to "/admin/applicants/#{applicant.id}"
   end
 
   private
   def pet_applicant_params
-    params.require(:pet_applicant).permit(:applicant_id, :pet_id)
+    params.require(:pet_applicant).permit(:applicant_id, :pet_id, :pet_status)
   end
 end
