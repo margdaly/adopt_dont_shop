cb93868575b525f5bb22e91ed9491c77a24f4ca0
margdaly <margaret.daly@gmail.com>
2023-04-05 17:22:47 -0600

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ Applicant.delete_all
 ## APPLICANTS    
     @heather = Applicant.create!(name: "Heather Moore", street: "Pearl St", city: "Denver", state: "CO", zip: "80210")
 
-    @olivia = Applicant.create!(name: "Olivia Valentin", street: "1234 Main St", city: "Denver", state: "CO", zip: "80203")
+    @olivia = Applicant.create!(name: "Olivia Valentin", street: "1234 Main St", city: "Denver", state: "CO", zip: "80203",  good_home: "Lots of love and land!", status: "Pending")
     PetApplicant.create!(applicant: @olivia, pet: @lucille_bald)
     PetApplicant.create!(applicant: @olivia, pet: @lobster)
 
